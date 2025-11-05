5245188bf19b1d431c3d09c69e8188fc2f914106
Johannes Burgmann <jburgma2@smail.uni-koeln.de>
2025-11-05 15:57:17 +0100

    test h1

--- src/h1/H1_main.java
+++ src/h1/H1_main.java
@@ -2,23 +2,28 @@ package h1;
 
 public class H1_main {
     public static void main(String[] args) {
-        double guthaben = 1200.79;
-        double monEingang = 2000;
+        double guthaben = -200;
+        double monEingang = 100;
         double guthabenBetrag = guthaben;
-        if (guthaben<0) guthabenBetrag*=-1;
+        if (guthaben < 0) guthabenBetrag *= -1;
         int rating = 0;
         boolean warnhinweis = false;
-        boolean negativ =false;
+        boolean negativ = false;
 
-        if (guthaben<0) negativ = true;
-        else if (guthaben==0) rating +=2;
-        else  rating +=3;
+        if (guthaben < 0) negativ = true;
+        else if (guthaben == 0) rating += 2;
+        else rating += 3;
 
-        if (negativ && monEingang>=guthabenBetrag) rating++;
-        else if (negativ && monEingang<=guthabenBetrag){
+        if (negativ && monEingang >= guthabenBetrag) rating++;
+        else if (negativ && monEingang <= guthabenBetrag) {
             rating--;
-            if (rating<0) warnhinweis=true;
-            else warnhinweis=false;
+            if (rating < 0) warnhinweis = true;
+            else warnhinweis = false;
         }
+        /*
+        System.out.println("Rating " + rating);
+        System.out.println("Negativ " + negativ);
+        System.out.println("Warnhinweis " + warnhinweis);
+         */
     }
 }
